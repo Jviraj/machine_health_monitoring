f9407282da80c39465e305b64e25973dc205bab4
Jviraj <virajjagadale123@gmail.com>
2023-03-22 16:06:34 +0530

    Added code to interface with the four sensors.

--- cad_project/cad_project.ino
+++ cad_project/cad_project.ino
@@ -1,7 +1,55 @@
+//mq2 gas sensor
+int LED = 32;            /*LED pin defined*/
+int Sensor_input = 4;    /*Digital pin 5 for sensor input*/
+//
+
+//For Ultrasonic sensor
+const int trigPin = 5;
+const int echoPin = 18;
+
+//define sound speed in cm/uS
+#define SOUND_SPEED 0.034
+#define CM_TO_INCH 0.393701
+
+long duration;
+float distanceCm;
+float distanceInch;
+//
+
+//For temperature sensor
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// GPIO where the DS18B20 is connected to
+const int oneWireBus = 4;     
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(oneWireBus);
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+//
+
+//For mq2 sensor
+int vib = 3;
+//
+
 void setup()
 {
     Serial.begin(115200);
-//    delay(1000);
+    delay(1000);
+    
+    //MQ2
+    pinMode(LED, OUTPUT);  /*LED set as Output*/
+
+    //Ultrasonic
+    pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+    pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+
+    //Temp
+    // Start the DS18B20 sensor
+    sensors.begin();
+    
     xTaskCreate(mq2, "Interfacing MQ2", 10000, NULL, 4, NULL);
     delay(2500);
     xTaskCreate(ultraSonic, "Interfacing HC SR04", 10000, NULL, 3, NULL);
@@ -17,7 +65,21 @@ void loop()
 void mq2(void *pvParameters)
 {
     while(1){
-    Serial.println("Inside MQ2");
+    //Serial.println("Inside MQ2");
+    int sensor_Aout = analogRead(Sensor_input);  /*Analog value read function*/
+    if(sensor_Aout<500) break;
+    Serial.print("Gas Sensor: ");    
+    Serial.print(sensor_Aout);   /*Read value printed*/
+    Serial.print("\t");
+    Serial.print("\t");
+    if (sensor_Aout > 1800) {    /*if condition with threshold 1800*/
+      Serial.println("Gas");  
+      digitalWrite (LED, HIGH) ; /*LED set HIGH if Gas detected */
+    }
+    else {
+      Serial.println("No Gas");
+      digitalWrite (LED, LOW) ;  /*LED set LOW if NO Gas detected */
+    }
     delay(10000);
     }
     vTaskDelete( NULL );
@@ -26,7 +88,31 @@ void mq2(void *pvParameters)
 void ultraSonic(void *pvParameters)
 {
     while(1){
-    Serial.println("Inside Ultrasonic");
+    //Serial.println("Inside Ultrasonic");
+    // Clears the trigPin
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    // Sets the trigPin on HIGH state for 10 micro seconds
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    duration = pulseIn(echoPin, HIGH);
+    
+    // Calculate the distance
+    distanceCm = duration * SOUND_SPEED/2;
+    
+    if(distanceCm==0) break;
+    
+    // Convert to inches
+    distanceInch = distanceCm * CM_TO_INCH;
+    
+    // Prints the distance in the Serial Monitor
+    Serial.print("Distance (cm): ");
+    Serial.println(distanceCm);
+    Serial.print("Distance (inch): ");
+    Serial.println(distanceInch);
     delay(10000);
     }
     vTaskDelete( NULL );
@@ -35,7 +121,15 @@ void ultraSonic(void *pvParameters)
 void temp(void *pvParameters)
 {
     while(1){
-    Serial.println("Inside Temperature");
+    //Serial.println("Inside Temperature");
+    sensors.requestTemperatures(); 
+    float temperatureC = sensors.getTempCByIndex(0);
+    float temperatureF = sensors.getTempFByIndex(0);
+    if(temperatureC<0) break;
+    Serial.print(temperatureC);
+    Serial.println("ºC");
+    Serial.print(temperatureF);
+    Serial.println("ºF");
     delay(10000);
     }
     vTaskDelete( NULL );
@@ -44,7 +138,15 @@ void temp(void *pvParameters)
 void vibration(void *pvParameters)
 {
     while(1){
-    Serial.println("Inside Vibration");
+    //Serial.println("Inside Vibration");
+    int value = pulseIn (vib, HIGH);
+    Serial.println(value);
+    if (value > 1000){
+    Serial.println("The Titans are coming!");
+    }
+    else{
+    Serial.println("Tranquility is here"); 
+    }
     delay(10000);
     }
     vTaskDelete( NULL );

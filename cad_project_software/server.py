1624c05fc8823251712b365471dbb52132d6015a
Sanskar Rajput <113208036+Rajputsanskar962@users.noreply.github.com>
2023-09-24 12:55:30 +0530

    Update server.py
    Backend functionality

--- cad_project_software/server.py
+++ cad_project_software/server.py
@@ -2,26 +2,36 @@ from flask import Flask, request, jsonify
 import threading
 import tkinter as tk
 from tkinter import font as tkFont
-import numpy as np
-import pandas as pd
+import psycopg2
+from datetime import datetime
+# from sklearn.neighbors import KNeighborsClassifier
 
-csv_file_path = "data.csv"
-dataset = pd.read_csv(csv_file_path)
+# Initialize the PostgreSQL database connection
+def initialize_database_connection():
+    connection = psycopg2.connect(
+        host="localhost",
+        user="sanskar",  # Replace with your PostgreSQL username
+        password="12345",  # Replace with your PostgreSQL password
+        database="mhmdb"
+    )
+    cursor = connection.cursor()
 
-X = dataset.drop('Failure', axis = 1)
-y = dataset['Failure']
+    create_table_query = """
+    CREATE TABLE IF NOT EXISTS sensor_data (
+        id SERIAL PRIMARY KEY,
+        timestamp VARCHAR(255),
+        mq2Gas VARCHAR(255),
+        distance VARCHAR(255),
+        temperature VARCHAR(255),
+        vibration VARCHAR(255)
+    )
+    """
+    cursor.execute(create_table_query)
+    connection.commit()
+    return connection
 
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1, random_state = 42)
-
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-
-X_train = sc.fit_transform(X_train)
-
-from sklearn.neighbors import KNeighborsClassifier
-classifierKNN = KNeighborsClassifier(n_neighbors=2, metric='minkowski', p=1)
-classifierKNN.fit(X_train, y_train)
+# Initialize the database connection
+db_connection = initialize_database_connection()
 
 app = Flask(__name__)
 
@@ -33,80 +43,132 @@ vibration = "0"
 
 global temperature_label, chatter_label, gas_leakage_label, coolant_level_label, temperature_led, chatter_led, gas_leakage_led, coolant_level_led, fail_safe_led
 
-
 @app.route("/greeting", methods=['POST'])
 def greeting():
     print("Hello")
     return jsonify("0"), 200
 
-
 @app.route("/update-mq2", methods=['POST'])
 def mq2():
-    global mq2Gas, distance, temperature
-    mq2Gas = request.json
+    sensor_data = request.json
+    mq2Gas = sensor_data.get('mq2Gas')
+    distance = sensor_data.get('distance')
+    temperature = sensor_data.get('temperature')
+    vibration = sensor_data.get('vibration')
+
+    # Get the current timestamp
+    timestamp = datetime.now()
+    timestamp_str = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+
+    # Insert the sensor data into the database
+    insert_query = "INSERT INTO sensor_data (timestamp, mq2Gas, distance, temperature, vibration) VALUES (%s, %s, %s, %s, %s)"
+    data_to_insert = (timestamp_str, mq2Gas, distance, temperature, vibration)
+
+    db_cursor = db_connection.cursor()
+    db_cursor.execute(insert_query, data_to_insert)
+    db_connection.commit()
+
     updateLabels()
     if mq2Gas == "1":
         updateLedMQ2(True)
     else:
         updateLedMQ2(False)
-    predict = threading.Thread(target=predictFailure, args = (mq2Gas, distance, temperature))
+    predict = threading.Thread(target=predictFailure, args=(mq2Gas, distance, temperature))
     predict.start()
     predict.join()
     print(f"Hello1: {mq2Gas}")
     return jsonify("1"), 200
 
-
 @app.route("/update-dist", methods=['POST'])
 def dist():
-    global mq2Gas, distance, temperature
-    distance = request.json
+    sensor_data = request.json
+    mq2Gas = sensor_data.get('mq2Gas')
+    distance = sensor_data.get('distance')
+    temperature = sensor_data.get('temperature')
+    vibration = sensor_data.get('vibration')
+
+    # Get the current timestamp
+    timestamp = datetime.now()
+    timestamp_str = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+    # Insert the sensor data into the database
+    insert_query = "INSERT INTO sensor_data (timestamp, mq2Gas, distance, temperature, vibration) VALUES (%s, %s, %s, %s, %s)"
+    data_to_insert = (timestamp_str, mq2Gas, distance, temperature, vibration)
+
+    db_cursor = db_connection.cursor()
+    db_cursor.execute(insert_query, data_to_insert)
+    db_connection.commit()
+
     updateLabels()
     if float(distance) > 30.0:
         updateLedCoolantLevel(True)
     else:
         updateLedCoolantLevel(False)
-    predict = threading.Thread(target=predictFailure, args = (mq2Gas, distance, temperature))
+    predict = threading.Thread(target=predictFailure, args=(mq2Gas, distance, temperature))
     predict.start()
     predict.join()
     print(f"Hello2: {distance}")
     return jsonify("2"), 200
 
-
 @app.route("/update-temp", methods=['POST'])
 def temp():
-    global mq2Gas, distance, temperature
-    temperature = request.json
+    sensor_data = request.json
+    mq2Gas = sensor_data.get('mq2Gas')
+    distance = sensor_data.get('distance')
+    temperature = sensor_data.get('temperature')
+    vibration = sensor_data.get('vibration')
+
+    # Get the current timestamp
+    timestamp = datetime.now()
+    timestamp_str = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+
+    # Insert the sensor data into the database
+    insert_query = "INSERT INTO sensor_data (timestamp, mq2Gas, distance, temperature, vibration) VALUES (%s, %s, %s, %s, %s)"
+    data_to_insert = (timestamp_str, mq2Gas, distance, temperature, vibration)
+
+    db_cursor = db_connection.cursor()
+    db_cursor.execute(insert_query, data_to_insert)
+    db_connection.commit()
+
     updateLabels()
     if float(temperature) > 25:
         updateLedTemperature(True)
     else:
         updateLedTemperature(False)
-    predict = threading.Thread(target=predictFailure, args = (mq2Gas, distance, temperature))
+    predict = threading.Thread(target=predictFailure, args=(mq2Gas, distance, temperature))
     predict.start()
     predict.join()
     print(f"Hello3: {temperature}")
     return jsonify("3"), 200
 
-
 @app.route("/update-vib", methods=['POST'])
 def vib():
-    global vibration
-    vibration = request.json
+    sensor_data = request.json
+    mq2Gas = sensor_data.get('mq2Gas')
+    distance = sensor_data.get('distance')
+    temperature = sensor_data.get('temperature')
+    vibration = sensor_data.get('vibration')
+
+    # Get the current timestamp
+    timestamp = datetime.now()
+    timestamp_str = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+    # Insert the sensor data into the database
+    insert_query = "INSERT INTO sensor_data (timestamp, mq2Gas, distance, temperature, vibration) VALUES (%s, %s, %s, %s, %s)"
+    data_to_insert = (timestamp_str, mq2Gas, distance, temperature, vibration)
+
+    db_cursor = db_connection.cursor()
+    db_cursor.execute(insert_query, data_to_insert)
+    db_connection.commit()
+
     updateLabels()
     if int(vibration) > 5:
         updateLedVibration(True)
     else:
         updateLedVibration(False)
-    # predict = threading.Thread(target=predictFailure())
-    # predict.start()
-    # predict.join()
     print(f"Hello4: {vibration}")
     return jsonify("4"), 200
 
-
 def server():
     app.run(port=8080, host='0.0.0.0')  # Set the address
-
 
 def updateLedMQ2(i):
     global gas_leakage_led
@@ -115,14 +177,12 @@ def updateLedMQ2(i):
     else:
         gas_leakage_led.config(bg="white")
 
-
 def updateLedVibration(i):
     global chatter_led
     if i == True:
         chatter_led.config(bg="red")
     else:
         chatter_led.config(bg="white")
-
 
 def updateLedTemperature(i):
     global temperature_led
@@ -131,14 +191,12 @@ def updateLedTemperature(i):
     else:
         temperature_led.config(bg="white")
 
-
 def updateLedCoolantLevel(i):
     global coolant_level_led
     if i == True:
         coolant_level_led.config(bg="red")
     else:
         coolant_level_led.config(bg="white")
-
 
 def updateLabels():
     global temperature, mq2Gas, vibration, distance, temperature_label, chatter_label, gas_leakage_label, coolant_level_label
@@ -150,7 +208,6 @@ def updateLabels():
     gas_leakage_label.config(text=f"Gas Leakage: {mq2Gas}", font=custom_font)
     coolant_level_label.config(
         text=f"Coolant Level: {distance}", font=custom_font)
-
 
 def win():
     global temperature_label, chatter_label, gas_leakage_label, coolant_level_label, temperature_led, chatter_led, gas_leakage_led, coolant_level_led, fail_safe_led
@@ -208,7 +265,7 @@ def predictFailure(m, d, t):
     if y == 1:
         fail_safe_led.config(bg="red")
     else:
-        fail_safe_led.config(bg="white")    
+        fail_safe_led.config(bg="white")
 
 if __name__ == '__main__':
     gui = threading.Thread(target=win)
